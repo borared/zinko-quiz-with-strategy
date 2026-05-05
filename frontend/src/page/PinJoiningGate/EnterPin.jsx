eb8920b1d61a359a1f354baef23ba47a70768eea
borared <boraofficial76@gmail.com>
2026-05-06 01:06:22 +0700

    feat(EntryPin): Add Join Game PIN entry screen
    Implemented the EnterPin section and corresponding page route to allow users to input a 6-digit class PIN. Replicated the provided neo-brutalist styling with a centered form card and floating background elements.

---
+++ frontend/src/page/PinJoiningGate/EnterPin.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import EnterPinSection from '../../components/EntryPin/EnterPinSection'
+export default function EnterPin() {
+    return (
+        <EnterPinSection />
+    )
+}
