6ebf8b0df95a83ccd8c69cd517bbfc461b13471b
borared <boraofficial76@gmail.com>
2026-05-06 23:38:54 +0700

    chore(App): Hide footer on join route and configure pricing route
    Updated App.jsx to use useLocation to conditionally hide the global Footer when the user is on the /join route. Configured the standalone /pricing route.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/global/Navbar";
 import Hero from "./page/landing/Hero";
 import Engagement from "./page/landing/Engagement";
@@ -9,6 +9,7 @@ import Footer from "./components/global/Footer";
 import EnterPin from "./page/PinJoiningGate/EnterPin";
 import Signup from "./components/Authentication/Signup";
 import Signin from "./components/Authentication/Signin";
+import PricingPanel from "./page/PricingPanel/PricingPanel";
 
 const LandingPage = () => (
   <>
@@ -20,6 +21,9 @@ const LandingPage = () => (
 );
 
 export default function App() {
+  const location = useLocation();
+  const showFooter = location.pathname !== '/join';
+
   return (
     <div className="min-h-screen bg-zk-yellow flex flex-col font-sans">
       <Navbar />
@@ -29,9 +33,10 @@ export default function App() {
           <Route path="/join" element={<EnterPin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/pricing" element={<PricingPanel />} />
         </Routes>
       </main>
-      <Footer />
+      {showFooter && <Footer />}
     </div>
   )
 }
