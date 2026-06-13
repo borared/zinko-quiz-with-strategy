a0dfd020f4779e52e6cd45063dd962584eb73db4
borared <boraofficial76@gmail.com>
2026-06-13 19:05:31 +0700

    feat(backend/models): introduce JSDoc type definitions for N-Tier data modeling

---
+++ backend/models/avatarModel.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} Avatar
+ * @property {number} id
+ * @property {string} name
+ * @property {string} image_url
+ * @property {number} rarity
+ * @property {Date} created_at
+ */
+
+module.exports = {};
---
+++ backend/models/quizModel.js
@@ -0,0 +1,25 @@
+/**
+ * @typedef {Object} Question
+ * @property {number} id
+ * @property {number} quiz_id
+ * @property {string} question_text
+ * @property {string|null} image_url
+ * @property {Object} answers - JSON object containing answer choices and correctness
+ * @property {number} order_index
+ * @property {number} round
+ */
+
+/**
+ * @typedef {Object} Quiz
+ * @property {number} id
+ * @property {string} title
+ * @property {string|null} description
+ * @property {string|null} cover_image
+ * @property {string} creator_id
+ * @property {boolean} is_public
+ * @property {Question[]} questions
+ * @property {Date} created_at
+ * @property {Date} updated_at
+ */
+
+module.exports = {};
