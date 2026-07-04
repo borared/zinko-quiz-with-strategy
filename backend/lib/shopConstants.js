f98eb48003fb154530403da3b1ea8acf38664f1f
borared <boraofficial76@gmail.com>
2026-07-04 23:47:48 +0700

    Add Shop Utils: Stripe configuration, price formatting, and shop constants

---
+++ backend/lib/shopConstants.js
@@ -0,0 +1,12 @@
+const SCENERY_PRICES_CENTS = {
+  halloween: 499,
+  inside: 299,
+  'ghost-station': 199,
+};
+
+const SHOP_CURRENCY = 'usd';
+
+module.exports = {
+  SCENERY_PRICES_CENTS,
+  SHOP_CURRENCY,
+};
