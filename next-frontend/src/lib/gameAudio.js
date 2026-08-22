dec6409e75871854a14079b257af28aba8866701
borared <boraofficial76@gmail.com>
2026-08-22 23:30:00 +0700

    Update Five Grid BGM to play Zinko default lobby-funk theme

--- next-frontend/src/lib/gameAudio.js
+++ next-frontend/src/lib/gameAudio.js
@@ -14,7 +14,7 @@ const GAME_AUDIO_CONFIG = {
     volume: 0.3,
   },
   fivegrid: {
-    src: '/audio/n2kstudio-music-for-game-fun-kid-game-163649.mp3',
+    src: '/audio/lobby-funk.mp3',
     volume: 0.3,
   },
   wheel: {
