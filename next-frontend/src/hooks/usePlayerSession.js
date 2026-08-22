d82ab907e908a660dd1f0c294127fe2618181982
borared <boraofficial76@gmail.com>
2026-08-22 17:38:47 +0700

    Refactor player session restore validation in usePlayerSession hook

--- next-frontend/src/hooks/usePlayerSession.js
+++ next-frontend/src/hooks/usePlayerSession.js
@@ -5,7 +5,9 @@ const PLAYER_SESSION_DEFAULTS = {
   nickname: 'Player',
   playerSkill: null,
   team: 'A',
+  avatar: 'pizza',
   isLeader: false,
+  isLoaded: false,
 };
 
 export function usePlayerSession() {
@@ -17,7 +19,9 @@ export function usePlayerSession() {
       nickname: sessionStorage.getItem('player_nickname') || PLAYER_SESSION_DEFAULTS.nickname,
       playerSkill: sessionStorage.getItem('player_skill') || PLAYER_SESSION_DEFAULTS.playerSkill,
       team: sessionStorage.getItem('player_team') || PLAYER_SESSION_DEFAULTS.team,
+      avatar: sessionStorage.getItem('player_avatar') || PLAYER_SESSION_DEFAULTS.avatar,
       isLeader: sessionStorage.getItem('player_is_leader') === 'true',
+      isLoaded: true,
     });
   }, []);
 
